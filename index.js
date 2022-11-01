a829ee0712de5a1d0d7f29c2daeb898977195467
benkin <bennykindangen@gmail.com>
2022-11-01 23:39:07 +0800

    moved frontend url to env

--- index.js
+++ index.js
@@ -20,13 +20,22 @@ app.use(
 expressBusboy.extend(app)
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept  ")
-    next();
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept  "
+    )
+    next()
 })
 
-app.use(cors({ origin: "http://localhost:3000", withCredentials: true, credentials: 'include' }))
+app.use(
+    cors({
+        origin: process.env.FRONT_URL,
+        withCredentials: true,
+        credentials: "include",
+    })
+)
 
 app.use("/api", router)
 
