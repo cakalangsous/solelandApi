b4c30c94db5e8eb6368f8170082b18aef245ec45
benkin <bennykindangen@gmail.com>
2022-11-02 01:48:16 +0800

    modified res cookie

--- controllers/parent/ParentAuthController.js
+++ controllers/parent/ParentAuthController.js
@@ -64,7 +64,6 @@ exports.login = async (req, res) => {
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
             secure: true,
         })
